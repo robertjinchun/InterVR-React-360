5199e9c0b2f81ae8206632220488399ff344d1f6
LissaNgSang <l.ngsang@gmail.com>
2019-09-12 18:55:41 -0400

    changed image

--- client.js
+++ client.js
@@ -1,14 +1,12 @@
 // This file contains the boilerplate to execute your React app.
 // If you want to modify your application's content, start in "index.js"
 
-import {ReactInstance} from 'react-360-web';
+import { ReactInstance } from "react-360-web";
 
 function init(bundle, parent, options = {}) {
-
   // initialise instant game
   if (FBInstant) {
-    FBInstant.initializeAsync()
-    .then(function() {
+    FBInstant.initializeAsync().then(function() {
       FBInstant.setLoadingProgress(100);
       FBInstant.startGameAsync();
     });
@@ -17,17 +15,19 @@ function init(bundle, parent, options = {}) {
   const r360 = new ReactInstance(bundle, parent, {
     // Add custom options here
     fullScreen: true,
-    ...options,
+    ...options
   });
 
   // Render your app content to the default cylinder surface
   r360.renderToSurface(
-    r360.createRoot('BasicAppTemplate', { /* initial props */ }),
+    r360.createRoot("BasicAppTemplate", {
+      /* initial props */
+    }),
     r360.getDefaultSurface()
   );
 
   // Load the initial environment
-  r360.compositor.setBackground(r360.getAssetURL('360_world.jpg'));
+  r360.compositor.setBackground(r360.getAssetURL("Zen360.jpg"));
 }
 
-window.React360 = {init};
+window.React360 = { init };
